85ca7d07945b8f4dc2b71f8397500d44cc6971d2
drinconcam364 <danilo.rinconcamacho@duke.edu>
2025-11-11 20:42:25 -0500

    Update FDPMAC.v

--- FDPMAC.v
+++ FDPMAC.v
@@ -1,12 +1,11 @@
-module FDPMAC(
-    rs1, rs2, rs3, clk);
+module FDPMAC(rs1, rs2, rs3, clk, out);
     input [31:0] rs1, rs2, rs3;
     input clk;
     output reg [31:0] out;
-    wire sign_a, sign_b, sign_c, sign_d, acc_sign;
+    wire sign_a, sign_b, sign_c, sign_d;
     wire [4:0] exponent;
     wire [10:0] mantissa_a, mantissa_b, mantissa_c, mantissa_d;
-    // Align a, b, c, and c with same exponent block so all mantissas can be added together and have a fused partial product wallace tree
+    // Align a, b, c, and d with same exponent block so all mantissas can be added together and have a fused partial product wallace tree
     align_exps align(
         .a(rs1[15:0]), 
         .b(rs1[31:16]), 
@@ -22,6 +21,7 @@ module FDPMAC(
         .mantissa_c_scaled(mantissa_c), 
         .mantissa_d_scaled(mantissa_d)
     );
+    // Obtain partial products from a * b
     wire signed[23:0] pp0, pp1, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11;
     radix4 ab(
         .multiplicand(mantissa_a), 
@@ -33,6 +33,7 @@ module FDPMAC(
         .pp4(pp4), 
         .pp5(pp5)
     );
+    // Obtain partial products from c * d
     radix4 cd(
         .multiplicand(mantissa_c), 
         .multiplier(mantissa_d), 
@@ -43,7 +44,8 @@ module FDPMAC(
         .pp4(pp10), 
         .pp5(pp11)
     );
-    wire [23:0] wallace_sum, wallace_carry, mantissa_ab_cd_sum;
+    // Use wallace tree to reduce the 12 partial products from a*b + c*d into a sum and carry
+    wire [23:0] wallace_sum, wallace_carry;
     wallace_12x24 wallace(
         .pp0(pp0), 
         .pp1(pp1), 
@@ -60,24 +62,47 @@ module FDPMAC(
         .sum(wallace_sum), 
         .carry(wallace_carry)
     );
-    // Once wallace outputs sum and carry, use CLA adder to add sum and carry, giving final mantissa, then normalize, round, and pack 
-    assign mantissa_ab_cd_sum = wallace_sum + (wallace_carry << 1);
-    wire [7:0] abcd_32_exponent, acc_exp;
-    wire [23:0] abcd_32_mantissa, acc_mantissa;
-    normalize norm(
-        .mantissa_in(mantissa_ab_cd_sum), 
-        .exp_in(exponent), 
-        .mantissa_out(abcd_32_mantissa), 
-        .exp_out(abcd_32_exponent)
-    );
+    localparam [7:0] FP32_BIAS = 8'd127;
+    localparam [4:0] FP16_BIAS = 5'd15;
+    wire [7:0] prod_exp;
+    assign prod_exp = FP32_BIAS + {3'b000, exponent} - {3'b000, FP16_BIAS};
+    wire acc_sign;
+    wire [7:0] acc_exp, final_exp;
+    wire[23:0] acc_mantissa, acc_mantissa_scaled;
     fp32_unpack acc_unpack(
         .in(rs3),
         .sign(acc_sign),
         .exp(acc_exp),
         .mantissa(acc_mantissa)
     );
-
-
-
-
+    wire [7:0] shift_prod;
+    //Align exponents of accumulator and wallace sum and carry so that they can all 3 be added together in CSA
+    align_acc aa(
+        .acc_exp(acc_exp),
+        .prod_exp(prod_exp),
+        .acc_mantissa(acc_mantissa),
+        .acc_mantissa_scaled(acc_mantissa_scaled),
+        .final_exp(final_exp),
+        .shift_prod(shift_prod)
+    );
+    wire [23:0] wallace_sum_scaled, wallace_carry_scaled;
+    wire [24:0] mantissa_final;
+    assign wallace_sum_scaled = wallace_sum >> shift_prod;
+    assign wallace_carry_scaled = wallace_carry >> shift_prod;
+    prod_acc blahhhh(
+        .wallace_sum_scaled(wallace_sum_scaled),
+        .wallace_carry_scaled(wallace_carry_scaled),
+        .acc_mantissa_scaled(acc_mantissa_scaled),
+        .mantissa_final(mantissa_final)
+    );
+    wire[31:0] final_result;
+    normalize norm(
+        .mantissa_in(mantissa_final), 
+        .exp_in(final_exp), 
+        .sign_in(1'b0),
+        .out(final_result)
+    );
+    always @(posedge clk) begin
+        out <= final_result;
+    end
 endmodule
