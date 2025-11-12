c444afab22581a8ba6b8af574c9df9ae1edfbe09
drinconcam364 <danilo.rinconcamacho@duke.edu>
2025-11-11 20:47:34 -0500

    Update fp32_unpack.v

--- fp32_unpack.v
+++ fp32_unpack.v
@@ -1,10 +1,10 @@
-module fp32_upack(in, sign, exp, mantissa);
+module fp32_unpack(in, sign, exp, mantissa);
     input [31:0] in;
     output sign;
     output [7:0] exp;
-    output [10:0] mantissa;
+    output [23:0] mantissa;
     assign sign = in[31];
     assign exp = in[30:23];
-    assign mantissa = (exp == 5'b0) ? {1'b0, in[22:0]} : {0'b0, in[22:0]};
+    assign mantissa = (exp == 8'b0) ? {1'b0, in[22:0]} : {1'b1, in[22:0]};
 endmodule
 
